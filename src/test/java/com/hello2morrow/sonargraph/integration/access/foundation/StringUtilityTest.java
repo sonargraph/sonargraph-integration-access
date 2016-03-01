a33b8d96d36138a0ecaa3a62ee112e6ccb753e9f
Ingmar <i.kellner@hello2morrow.com>
2016-03-01 14:41:21 +0100

    Generate license header into source files.

--- src/test/java/com/hello2morrow/sonargraph/integration/access/foundation/StringUtilityTest.java
+++ src/test/java/com/hello2morrow/sonargraph/integration/access/foundation/StringUtilityTest.java
@@ -1,7 +1,7 @@
-/*******************************************************************************
- * Jenkins Sonargraph Plugin
- * Copyright (C) 2009-2015 hello2morrow GmbH
- * mailto: info AT hello2morrow DOT com
+/**
+ * Sonargraph Integration Access
+ * Copyright (C) 2016 hello2morrow GmbH
+ * mailto: support AT hello2morrow DOT com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,9 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *******************************************************************************/
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hello2morrow.sonargraph.integration.access.foundation;
 
 import static org.junit.Assert.assertEquals;
