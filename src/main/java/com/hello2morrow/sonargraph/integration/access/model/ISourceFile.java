b5f320271cb558457ff5fa77aca1d9db037b27e9
sascha <a.zitzewitz@hello2morrow.com>
2018-06-12 15:47:03 -0400

    Updated license headers

--- src/main/java/com/hello2morrow/sonargraph/integration/access/model/ISourceFile.java
+++ src/main/java/com/hello2morrow/sonargraph/integration/access/model/ISourceFile.java
@@ -1,6 +1,6 @@
 /**
  * Sonargraph Integration Access
- * Copyright (C) 2016-2017 hello2morrow GmbH
+ * Copyright (C) 2016-2018 hello2morrow GmbH
  * mailto: support AT hello2morrow DOT com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
